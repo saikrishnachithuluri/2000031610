727243f0197abfd021f148dcc0a8046952daac88
saikrishnachithuluri <135578547+saikrishnachithuluri@users.noreply.github.com>
2023-06-12 11:15:41 +0530

    Delete TrainsSchedules directory

--- TrainsSchedules/src/main/java/com/klef/jfsd/springbootingboot/TrainService.java
+++
@@ -1,8 +0,0 @@
-package com.klef.jfsd.springbootingboot;
-
-import java.util.List;
-
-public interface TrainService {
-    List<Train> getAllTrains();
-    Train getTrainByNumber(String trainNumber);
-}
